7a799a7e64d05c058d11383a60f9efecbeb880a1
Albert Agram <albert.agram@gmail.com>
2018-06-26 15:08:16 +0000

    User model and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :josn } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+
+    resource :user, only: [:show, :update]
   end
 end
