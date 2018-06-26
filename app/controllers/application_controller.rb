06855cd0c32e584af7bdf5f8c49a213b2fa0632e
Albert Agram <albert.agram@gmail.com>
2018-06-26 12:04:35 +0000

    api config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :underscore_params!
   protect_from_forgery with: :null_session
+  respond_to :json
+
+  private
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
 end
