b22bbf363af9036b52e6832668f414596b41bb8e
Albert Agram <albert.agram@gmail.com>
2018-06-26 22:07:59 +0000

    fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
-  has_many: articles, dependent: :destroy
+  has_many :articles, dependent: :destroy
 
   def generate_jwt
     JWT.encode({ id: id,
