d48c5e4a4999178c1682b9a45dd882da8d6ce941
Albert Agram <albert.agram@gmail.com>
2018-06-27 12:43:05 +0000

    user favorite/unfavorite an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
 
     @articles = @articles.authored_by(params[:author]) if params[:author].present?
 
+    @articles = @articles.favorited_by(params[:favorited]) if params[:favorited].present?
+
     @articles_count = @articles.count
 
     @articles = @articles.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
-  scope :authored_by, ->(username) { where(user: User.where(username: username)) }
+  scope :authored_by, -> (username) { where(user: User.where(username: username)) }
+  scope :favorited_by, -> (username) { joins(:favorites).where(favorites: { user: User.where(username: username) }) }
 
   acts_as_taggable
 
