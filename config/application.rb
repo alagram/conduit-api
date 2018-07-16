dc90dd64a0998a6976870a80394041a651b4b44d
Albert Agram <albert.agram@gmail.com>
2018-07-16 19:40:43 +0000

    add delete method to rack-cors config

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module ConduitApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options, :delete]
       end
     end
 
